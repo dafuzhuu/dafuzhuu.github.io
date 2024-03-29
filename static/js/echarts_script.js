30edb49239dac500b6f591250c0bb4d6de9a7bf4
zzzzdf <156023557+ultimate233@users.noreply.github.com>
2024-03-30 02:44:22 +0800

    Update echarts_script.js

--- static/js/echarts_script.js
+++ static/js/echarts_script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
       trigger: 'axis'
     },
     legend: {
-      data: ['我的组合', '沪深300', '上证指数']
+      data: ['我的组合', '沪深300', '上证指数', '纳斯达克', '日经225']
     },
     grid: {
       left: '3%',
@@ -25,15 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
       bottom: '3%',
       containLabel: true
     },
-    toolbox: {
-      feature: {
-        saveAsImage: {}
-      }
-    },
     xAxis: {
       type: 'category',
       boundaryGap: false,
-      data: ['2024.4', '5', '6', '7', '8', '9', '10']
+      data: ['2024.4', '5', '6', '7', '8', '9', '10', '11', '12', '2025.1']
     },
     yAxis: {
       type: 'value'
@@ -43,19 +38,31 @@ document.addEventListener('DOMContentLoaded', function() {
         name: '我的组合',
         type: 'line',
         stack: null,
-        data: [100, 100, 100, 100, 100, 100, 100]
+        data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
       },
       {
         name: '沪深300',
         type: 'line',
         stack: null,
-        data: [100, 100, 100, 100, 100, 100, 100]
+        data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
       },
       {
         name: '上证指数',
         type: 'line',
         stack: null,
-        data: [100, 100, 100, 100, 100, 100, 100]
+        data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+      },
+      {
+        name: '纳斯达克',
+        type: 'line',
+        stack: null,
+        data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+      },
+      {
+        name: '日经225',
+        type: 'line',
+        stack: null,
+        data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
       }
     ]
   };
