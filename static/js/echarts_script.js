ff68bcd08ad8aa1a9e72e862e53d1d021c7ac843
zzzzdf <156023557+ultimate233@users.noreply.github.com>
2024-03-29 22:05:10 +0800

    Create echarts_script.js

---
+++ static/js/echarts_script.js
@@ -0,0 +1,65 @@
+// echarts_script.js
+
+// 在页面加载完成后执行以下代码
+document.addEventListener('DOMContentLoaded', function() {
+  // 获取图表容器
+  var chartDom = document.getElementById('main');
+  // 初始化 echarts 实例
+  var myChart = echarts.init(chartDom);
+  var option;
+
+  // 配置项
+  option = {
+    title: {
+      text: '收益曲线'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data: ['我的组合', '沪深300', '上证指数']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['2024.4', '5', '6', '7', '8', '9', '10']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name: '我的组合',
+        type: 'line',
+        stack: null,
+        data: [100, 100, 100, 100, 100, 100, 100]
+      },
+      {
+        name: '沪深300',
+        type: 'line',
+        stack: null,
+        data: [100, 100, 100, 100, 100, 100, 100]
+      },
+      {
+        name: '上证指数',
+        type: 'line',
+        stack: null,
+        data: [100, 100, 100, 100, 100, 100, 100]
+      }
+    ]
+  };
+
+  // 设置配置项并渲染图表
+  option && myChart.setOption(option);
+});
