63ca5abab725783adb62ddccc8800b184679c2e9
zzzzdf <156023557+ultimate233@users.noreply.github.com>
2024-03-30 02:54:09 +0800

    Update echarts_script.js

--- static/js/echarts_script.js
+++ static/js/echarts_script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
       trigger: 'axis'
     },
     legend: {
-      data: ['我的组合', '沪深300', '上证指数', '纳斯达克', '日经225']
+      data: ['我的组合', '沪深300', '上证指数', '纳斯达克', '标普500', '日经225']
     },
     grid: {
       left: '3%',
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     xAxis: {
       type: 'category',
       boundaryGap: false,
-      data: ['2024.4', '5', '6', '7', '8', '9', '10', '11', '12', '2025.1']
+      data: ['2024.4', '2024.5', '2024.6', '2024.7', '2024.8', '2024.9', '2024.10', '2024.11', '2024.12', '2025.1']
     },
     yAxis: {
       type: 'value'
@@ -54,6 +54,12 @@ document.addEventListener('DOMContentLoaded', function() {
       },
       {
         name: '纳斯达克',
+        type: 'line',
+        stack: null,
+        data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+      },
+      {
+        name: '标普500',
         type: 'line',
         stack: null,
         data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
