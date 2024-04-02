0d6f8cdf2b96e2e00cf1031f1c96f86dcf4dc7d4
zzzzdf <156023557+ultimate233@users.noreply.github.com>
2024-04-02 15:08:47 +0800

    Update echarts_script.js

--- static/js/echarts_script.js
+++ static/js/echarts_script.js
@@ -2,11 +2,11 @@
 
 // 数据
 const name = ['因子选股', 'ETF轮动', '沪深300', '上证指数'];
-const dates = ['2024-04-01'];
-const data_factor = [1.64];
-const data_etf = [0.57];
-const data_1A0300 = [1.64];
-const data_1A0001 = [1.19];
+const dates = ['2024-04-01', '2024-04-02'];
+const data_factor = [1.64, 1.93];
+const data_etf = [0.57, 0.08];
+const data_1A0300 = [1.64, -0.42];
+const data_1A0001 = [1.19, -0.08];
 
 // 累加函数
 function accumulate(inputArray) {
