7c0039b703209958173021ed20cdb4ab8ee27b2a
zzzzdf <156023557+ultimate233@users.noreply.github.com>
2024-04-02 11:43:58 +0800

    Update echarts_script.js

--- static/js/echarts_script.js
+++ static/js/echarts_script.js
@@ -1,27 +1,7 @@
 // echarts_script.js
 
-// import { name, dates, data_factor, data_etf, data_1A0300, data_1A0001 } from './config.js';
-// import { accumulate } from './utils.js';
-
-// config.js
-
-const name = ['因子选股', 'ETF轮动', '沪深300', '上证指数'];
-const dates = ['2024-04-01','2024-04-02'];
-const data_factor = [1.64,1];
-const data_etf = [0.57,1];
-const data_1A0300 = [1.64,1];
-const data_1A0001 = [1.19,1];
-
-// utils.js
-function accumulate(inputArray) {
-    let result = [];
-    let sum = 0;
-    for (let i = 0; i < inputArray.length; i++) {
-        sum += inputArray[i];
-        result.push(sum);
-    }
-    return result;
-}
+import { name, dates, data_factor, data_etf, data_1A0300, data_1A0001 } from './config.js';
+import { accumulate } from './utils.js';
 
 let result_factor = accumulate(data_factor);
 let result_etf = accumulate(data_etf);
