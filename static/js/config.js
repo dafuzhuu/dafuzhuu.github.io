ad8539e7dae6fc1b9ac6eece67808e143e49509c
zzzzdf <156023557+ultimate233@users.noreply.github.com>
2024-04-02 10:30:27 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,9 @@
 // config.js
 
+const name = ['因子选股', 'ETF轮动', '沪深300', '上证指数'];
+
+const dates = ['2024-04-01'];
+
 const data_factor = [1.64];
 
 const data_etf = [0.57];
@@ -8,4 +12,4 @@ const data_1A0300 = [1.64];
 
 const data_1A0001 = [1.19];
 
-export { data_factor, data_etf, data_1A0300, data_1A0001 };
+export { name, dates, data_factor, data_etf, data_1A0300, data_1A0001 };
