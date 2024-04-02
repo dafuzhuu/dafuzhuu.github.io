ab295e49a1626c0f907a7e9ad12c5647155fcbc5
zzzzdf <156023557+ultimate233@users.noreply.github.com>
2024-04-02 10:21:34 +0800

    Create config.js

---
+++ static/js/config.js
@@ -0,0 +1,11 @@
+// config.js
+
+const data_factor = [1.64];
+
+const data_etf = [0.57];
+
+const data_1A0300 = [1.64];
+
+const data_1A0001 = [1.19];
+
+export { data_factor, data_etf, data_1A0300, data_1A0001 };
