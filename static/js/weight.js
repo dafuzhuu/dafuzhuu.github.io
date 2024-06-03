073df3bf5254483d9e30610f14ce4d8eba734c23
zzzzdf <156023557+dafuzhuu@users.noreply.github.com>
2024-06-03 19:01:31 +0800

    Update weight.js

--- static/js/weight.js
+++ static/js/weight.js
@@ -24,7 +24,7 @@ option = {
   xAxis: {
     type: 'category',
     boundaryGap: false,
-    data: ['2024.5.5', '5.15', '5.24', '6.2']
+    data: ['2024.5.5', '5.15', '5.24', '6.2', '6.3']
   },
   yAxis: {
     type: 'value',
@@ -36,7 +36,7 @@ option = {
   series: [
     {
       type: 'line',
-      data: [81.03, 79.81, 78.11, 79.37],
+      data: [81.03, 79.81, 78.11, 79.37, 79.16],
       smooth: true
     }
   ]
